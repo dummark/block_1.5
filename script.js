72e6c200bb96f854e0c35780c387e6ba9b541459
Mark Dumler <3162mol@gmail.com>
2024-09-27 02:24:13 +0300

    ebus so swiperom

---
+++ script.js
@@ -0,0 +1,49 @@
+/*  new Swiper('.logo-swiper', {
+	slidesPerView: 'auto',
+	spaceBetween: 20,
+	freeMode: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+		pageUpDown: true,
+	},
+	mousewheel: {
+		sensitivity: 1,
+	},
+}) */
+
+var init = false
+var swiper
+function swiperCard() {
+	if (window.innerWidth <= 768) {
+		if (!init) {
+			init = true
+			swiper = new Swiper('.logo-swiper', {
+				slidesPerView: 'auto',
+				spaceBetween: 20,
+				freeMode: true,
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				keyboard: {
+					enabled: true,
+					onlyInViewport: true,
+					pageUpDown: true,
+				},
+				mousewheel: {
+					sensitivity: 1,
+				},
+			})
+		}
+	} else if (init) {
+		swiper.destroy()
+		init = false
+	}
+}
+swiperCard()
+window.addEventListener('resize', swiperCard)
