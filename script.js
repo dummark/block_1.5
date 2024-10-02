36f9bd91596d827448d516b5f11b5beeb2674049
Mark Dumler <3162mol@gmail.com>
2024-10-02 21:28:45 +0300

    finish

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-/*  new Swiper('.logo-swiper', {
-	slidesPerView: 'auto',
-	spaceBetween: 20,
-	freeMode: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true,
-		pageUpDown: true,
-	},
-	mousewheel: {
-		sensitivity: 1,
-	},
-}) */
-
 var init = false
 var swiper
 function swiperCard() {
-	if (window.innerWidth <= 768) {
+	if (window.innerWidth < 768) {
 		if (!init) {
 			init = true
-			swiper = new Swiper('.logo-swiper', {
+			swiper = new Swiper('.swiper', {
 				slidesPerView: 'auto',
 				spaceBetween: 20,
 				freeMode: true,
@@ -47,3 +29,21 @@ function swiperCard() {
 }
 swiperCard()
 window.addEventListener('resize', swiperCard)
+
+const toggleButton = document.querySelector('.toggleButton')
+const swiperHeight = document.querySelectorAll('.swiper')
+const arrows = document.querySelector('.arrows')
+
+toggleButton.addEventListener('click', () => {
+	swiperHeight.forEach(item => {
+		if (toggleButton.textContent == 'Скрыть') {
+			item.style.height = '170px'
+			toggleButton.textContent = 'Показать все'
+			arrows.src = 'images/arrows.svg'
+		} else {
+			item.style.height = 'auto'
+			toggleButton.textContent = 'Скрыть'
+			arrows.src = 'images/arrowsreverse.svg'
+		}
+	})
+})
